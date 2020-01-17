7dc8d900b58ee3d827f3631d587070b300958ab7
Student <example@example.com>
2020-01-17 15:38:38 -0800

    update to seeding db

--- rails_marios_reviews/db/seeds.rb
+++ rails_marios_reviews/db/seeds.rb
@@ -6,28 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
+Review.destroy_all
+
+
 
 50.times do |index|
   Product.create!(name: Faker::Books::Lovecraft.tome, #=> "Book of Eibo
-                        country_of_origin: "United Kingdom",
-                        cost: 5.00)
+    country_of_origin: "United Kingdom",
+    cost: 5.00)
+  end
 
-end
-
-p "Created #{Product.count} products"
-
-Review.destroy_all
-
-30.times do |index|
-  Review.create!(name: Faker::Books::Lovecraft.tome, #=> "Book of Eibo
-                        country_of_origin: "United Kingdom",
-                        cost: 5.00)
-
-end
-
-
-Faker.random.number(100
-  250.times do |index|
-    Review.product.create!(name: Faker::Books::Lovecraft.tome, #=> "Book of Eibo
-                          country_of_origin: "United Kingdom",
-                          cost: 5.00)
+  p "Created #{Product.count} products"
