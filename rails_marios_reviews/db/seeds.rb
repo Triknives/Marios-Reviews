626beab2c0c220d43b139f5a96bc8cb1f1f58bdd
Student <example@example.com>
2020-01-17 14:58:58 -0800

    update to faker seeding and flash messages

--- rails_marios_reviews/db/seeds.rb
+++ rails_marios_reviews/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Books::Lovecraft.tome, #=> "Book of Eibo
+                        country_of_origin: "United Kingdom",
+                        cost: 5.00)
+
+end
+
+p "Created #{Product.count} products"
+
+Review.destroy_all
+
+30.times do |index|
+  Review.create!(name: Faker::Books::Lovecraft.tome, #=> "Book of Eibo
+                        country_of_origin: "United Kingdom",
+                        cost: 5.00)
+
+end
+
+
+Faker.random.number(100
+  250.times do |index|
+    Review.product.create!(name: Faker::Books::Lovecraft.tome, #=> "Book of Eibo
+                          country_of_origin: "United Kingdom",
+                          cost: 5.00)
